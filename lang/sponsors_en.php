8cfcd9d07723270ae229c2ec66571af300996e42
moneermax <120671131+moneermax@users.noreply.github.com>
2026-09-04 23:30:18 +0300

    i18n: add sponsor assignment translations

---
+++ lang/sponsors_en.php
@@ -0,0 +1,4 @@
+<?php
+return [
+'sponsors.reassign_title'=>'Reassign Sponsors','sponsors.reassign_intro'=>'Unassigned sponsors are ready to be assigned to an active supervisor.','sponsors.code'=>'Code','sponsors.sponsor'=>'Sponsor','sponsors.new_supervisor'=>'New Supervisor','sponsors.assign'=>'Assign','sponsors.select_supervisor'=>'Select supervisor','sponsors.no_unassigned'=>'There are no unassigned sponsors.','sponsors.reassigned'=>'Sponsor reassigned to the supervisor successfully.','sponsors.reassign_save_error'=>'Unable to save the reassignment.','sponsors.not_found'=>'Sponsor not found.','sponsors.supervisor_invalid'=>'The selected supervisor is inactive or does not exist.','sponsors.session_expired'=>'Session expired.'
+];
