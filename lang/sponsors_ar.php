a35af4c544cfea630e87e3194cb596c68213c9af
moneermax <120671131+moneermax@users.noreply.github.com>
2026-09-04 23:30:14 +0300

    i18n: add sponsor assignment translations

---
+++ lang/sponsors_ar.php
@@ -0,0 +1,4 @@
+<?php
+return [
+'sponsors.reassign_title'=>'إعادة توزيع الكفلاء','sponsors.reassign_intro'=>'الكفلاء غير المعيّنين جاهزون لإسنادهم إلى مشرف نشط.','sponsors.code'=>'الكود','sponsors.sponsor'=>'الكفيل','sponsors.new_supervisor'=>'المشرف الجديد','sponsors.assign'=>'إسناد','sponsors.select_supervisor'=>'اختر المشرف','sponsors.no_unassigned'=>'لا يوجد كفلاء غير معيّنين.','sponsors.reassigned'=>'تمت إعادة تعيين الكفيل إلى المشرف بنجاح.','sponsors.reassign_save_error'=>'تعذر حفظ إعادة التعيين.','sponsors.not_found'=>'الكفيل غير موجود.','sponsors.supervisor_invalid'=>'المشرف المختار غير نشط أو غير موجود.','sponsors.session_expired'=>'انتهت صلاحية الجلسة.'
+];
