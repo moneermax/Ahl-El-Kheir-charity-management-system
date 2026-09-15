0b12672872e30c40486e42d7519dda30dd284471
moneermax <120671131+moneermax@users.noreply.github.com>
2026-09-15 08:07:32 +0300

    Add searchable sponsorship selector to payment entry

--- includes/footer.php
+++ includes/footer.php
@@ -43,6 +43,7 @@ window.AK_TRANSLATIONS=<?php echo json_encode(ak_dict(),JSON_UNESCAPED_UNICODE|J
 <script src="<?php echo asset('js/messaging_ui_cleanup.js'); ?>"></script>
 <script src="<?php echo asset('js/messaging_attachments.js'); ?>"></script>
 <script src="<?php echo asset('js/messaging_message_delete.js'); ?>"></script>
+<script src="<?php echo asset('js/searchable_sponsorship_select.js'); ?>"></script>
 <?php if (($active ?? '') === 'fm_dashboard'): ?><script src="<?php echo asset('js/fm_dashboard_layout.js'); ?>"></script><?php endif; ?>
 <?php if (($active ?? '') === 'fm_dashboard'): ?>
 <?php endif; ?>
