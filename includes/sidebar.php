754a53b467e8e027543c1e08925ae5e74586b10e
moneermax <120671131+moneermax@users.noreply.github.com>
2026-09-18 12:27:01 +0300

    Add winback workflow to administration navigation

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -110,6 +110,7 @@ $menus = [
         ['active' => 'dashboard', 'label_key' => 'navigation.dashboard', 'icon' => 'fa-gauge-high', 'url' => 'dashboard/staff_dashboard.php'],
         ['active' => 'requests', 'label_key' => 'navigation.join_requests', 'icon' => 'fa-user-plus', 'url' => 'modules/sponsors/requests.php'],
         $orphanFormsItem,
+        $winbackItem,
     ],
 
     'staff' => [
