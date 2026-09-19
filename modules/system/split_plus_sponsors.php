1e3e41827016703895641d19cc5e85ecb0c21aa8
moneermax <120671131+moneermax@users.noreply.github.com>
2026-09-19 17:27:09 +0300

    Add contextual Back button

--- modules/system/split_plus_sponsors.php
+++ modules/system/split_plus_sponsors.php
@@ -193,4 +193,6 @@ include dirname(__DIR__, 2) . '/includes/header.php';
 </div>
 </div>
 </div>
+
+<div class="container-fluid px-3 pb-4"><div class="d-flex justify-content-start"><a href="<?php echo APP_URL+'modules/settings/index.php'; ?>" class="btn btn-outline-secondary" onclick="return akGoBack(this.href);"><i class="fa-solid fa-arrow-right me-1"></i> العودة</a></div></div>
 <?php include dirname(__DIR__, 2) . '/includes/footer.php'; ?>
