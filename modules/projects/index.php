57f0c5eb3b132c34057f6c910b1a5f790d74f5f7
moneermax <120671131+moneermax@users.noreply.github.com>
2026-09-19 17:24:08 +0300

    Add contextual Back button to user-facing list page

--- modules/projects/index.php
+++ modules/projects/index.php
@@ -111,4 +111,6 @@ include dirname(__DIR__, 2) . '/includes/header.php';
 </td></tr>
 <?php endforeach; endif; ?>
 </tbody></table></div></div></div>
+
+<div class="container-fluid px-3 pb-4"><div class="d-flex justify-content-start"><a href="<?php echo APP_URL; ?>" class="btn btn-outline-secondary" onclick="return akGoBack(this.href);"><i class="fa-solid fa-arrow-right me-1"></i> العودة</a></div></div>
 <?php include dirname(__DIR__, 2) . '/includes/footer.php'; ?>
