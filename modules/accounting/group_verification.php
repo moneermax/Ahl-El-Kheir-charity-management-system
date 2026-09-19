f6b2d3491f40b4a71ddd1622c4e096bc21185c32
moneermax <120671131+moneermax@users.noreply.github.com>
2026-09-19 17:23:03 +0300

    Add contextual Back button to accounting page

--- modules/accounting/group_verification.php
+++ modules/accounting/group_verification.php
@@ -397,6 +397,8 @@ include dirname(__DIR__, 2) . '/includes/header.php';
     <?php endif; ?>
 </div>
 
+
+<div class="container-fluid px-3 pb-4"><div class="d-flex justify-content-start"><a href="<?php echo APP_URL; ?>modules/accounting/index.php" class="btn btn-outline-secondary" onclick="return akGoBack(this.href);"><i class="fa-solid fa-arrow-right me-1"></i> العودة</a></div></div>
 <?php include dirname(__DIR__, 2) . '/includes/footer.php'; ?>
 
 <!-- SweetAlert2 for confirmation -->
