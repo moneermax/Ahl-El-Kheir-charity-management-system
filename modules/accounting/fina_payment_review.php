0f8593d725b5922d4d7fff0ba3bfe17dfc7c2745
moneermax <120671131+moneermax@users.noreply.github.com>
2026-09-19 17:22:35 +0300

    Add contextual Back button to accounting/report page

--- modules/accounting/fina_payment_review.php
+++ modules/accounting/fina_payment_review.php
@@ -309,4 +309,6 @@ include dirname(__DIR__, 2) . '/includes/header.php';
         </div>
     </div>
 </div>
+
+<div class="container-fluid px-3 pb-4"><div class="d-flex justify-content-start"><a href="<?php echo APP_URL; ?>modules/accounting/index.php" class="btn btn-outline-secondary" onclick="return akGoBack(this.href);"><i class="fa-solid fa-arrow-right me-1"></i> العودة</a></div></div>
 <?php include dirname(__DIR__, 2) . '/includes/footer.php'; ?>
