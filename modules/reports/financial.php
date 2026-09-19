ab1081ab768f3b83527951393546aba5c27866e9
moneermax <120671131+moneermax@users.noreply.github.com>
2026-09-19 17:21:46 +0300

    Add contextual Back button to report page

--- modules/reports/financial.php
+++ modules/reports/financial.php
@@ -279,4 +279,6 @@ function exportToPDF(){
 }
 </script>
 
+
+<div class="container-fluid px-3 pb-4"><div class="d-flex justify-content-start"><a href="<?php echo APP_URL; ?>modules/reports/index.php" class="btn btn-outline-secondary" onclick="return akGoBack(this.href);"><i class="fa-solid fa-arrow-right me-1"></i> العودة</a></div></div>
 <?php include dirname(__DIR__, 2) . '/includes/footer.php'; ?>
