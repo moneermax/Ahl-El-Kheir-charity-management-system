e653b946dbaea3c92f4b677f6510cb0338c4e931
moneermax <120671131+moneermax@users.noreply.github.com>
2026-09-19 17:23:14 +0300

    Add contextual Back button to accounting page

--- modules/accounting/returned_funds_nanny.php
+++ modules/accounting/returned_funds_nanny.php
@@ -124,4 +124,6 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 </script>
+
+<div class="container-fluid px-3 pb-4"><div class="d-flex justify-content-start"><a href="<?php echo APP_URL; ?>modules/accounting/index.php" class="btn btn-outline-secondary" onclick="return akGoBack(this.href);"><i class="fa-solid fa-arrow-right me-1"></i> العودة</a></div></div>
 <?php include dirname(__DIR__, 2) . '/includes/footer.php'; ?>
