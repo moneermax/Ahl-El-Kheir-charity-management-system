ac72ff8417c3b0dcbfafe56a1b8d7119ef6903d3
moneermax <120671131+moneermax@users.noreply.github.com>
2026-09-19 17:26:15 +0300

    Fix HR Back button fallback URL

--- modules/hr/payroll_policy.php
+++ modules/hr/payroll_policy.php
@@ -165,5 +165,5 @@ require_once __DIR__ . '/../../includes/header.php';
 </div>
 
 
-<div class="container-fluid px-3 pb-4"><div class="d-flex justify-content-start"><a href="modules/hr/index.php" class="btn btn-outline-secondary" onclick="return akGoBack(this.href);"><i class="fa-solid fa-arrow-right me-1"></i> العودة</a></div></div>
+<div class="container-fluid px-3 pb-4"><div class="d-flex justify-content-start"><a href="<?php echo APP_URL; ?>modules/hr/index.php" class="btn btn-outline-secondary" onclick="return akGoBack(this.href);"><i class="fa-solid fa-arrow-right me-1"></i> العودة</a></div></div>
 <?php require_once __DIR__ . '/../../includes/footer.php'; ?>
