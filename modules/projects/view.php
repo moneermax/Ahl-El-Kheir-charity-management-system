f0a82f804a1a86d2055b315d076bc5767b0f6729
moneermax <120671131+moneermax@users.noreply.github.com>
2026-09-26 13:32:01 +0300

    Fix project view closure section PHP balance

--- modules/projects/view.php
+++ modules/projects/view.php
@@ -2421,5 +2421,4 @@ bootstrap.Toast.getOrCreateInstance(projectToast, {delay: 4500, autohide: true})
 });
 </script>
 <?php endif; ?>
-<?php endif; ?>
 <?php include dirname(__DIR__, 2) . '/includes/footer.php'; ?>
